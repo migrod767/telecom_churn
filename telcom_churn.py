3c32ae17b60a70430c21d497860422fa5403550d
migrod767 <mf.rodriguez12@uniandes.edu.co>
2020-04-15 22:03:39 -0500

    first commit

--- telcom_churn.py
+++ telcom_churn.py
@@ -1,35 +1,66 @@
 import pandas as pd
+import math
 
-def entropy(feature, dataset):
+
+def information_gain(feature, dataset):
+
+    if feature == "tenure" or feature == 'MonthlyCharges' or\
+            feature == 'TotalCharges' or feature=='Churn':
+        return 0
 
     values = dataset[feature].unique()
 
+    # Parent Entropy
+
+    true_values = sum(dataset['Churn'] == True)
+
+    total_obs = dataset.shape[0]
+
+    p_positive = true_values / total_obs
+    p_negative = 1 - p_positive
+
+    entropy_parent = -(p_positive * math.log2(p_positive) + p_negative * math.log2(p_negative))
+
+    # Information Gain
+
+    IG = entropy_parent
+
     for i in values:
-        temp_df = dataset[dataset[feature]==i]
+        temp_df = dataset[dataset[feature] == i]
 
-        True_values = dataset['Churn'] == True
+        p_child = temp_df.shape[0]/total_obs
 
-        #p_positive =
-        p_negative = 1- p_positive
+        true_values = sum(temp_df['Churn'] == True)
+
+        p_positive = true_values / temp_df.shape[0]
+        p_negative = 1 - p_positive
+
+        # Entropy Calc
+        entropy_child = -(p_positive * math.log2(p_positive) + p_negative * math.log2(p_negative))
+
+        # Information Gain Calc
+        IG = IG - (entropy_child*p_child)
+
+    return IG
 
 def load_clean_data():
     dataset = pd.read_csv("Data/Telco-Customer-Churn.csv")
-    dataset = dataset.drop(["customerID"], axis = 1)
+    dataset = dataset.drop(["customerID"], axis=1)
+
+    maping_Y_N = {'No': False, 'Yes': True}
+    maping_list = ['Churn', 'Partner', 'Dependents', 'PhoneService', 'PaperlessBilling']
+    for i in maping_list:
+        dataset[i] = dataset[i].map(maping_Y_N)
 
     data_top = dataset.columns
-
     for head in data_top:
         print("-------{}----------".format(head))
         print(dataset[head].unique())
+        print('IG: {}'.format(information_gain(head,dataset)))
         print("\n")
 
-    maping_Y_N = {'No':False, 'Yes':True}
-    dataset = dataset['Churn'].map(maping_Y_N)
+    x = 1
 
-    entropy('gender', dataset)
-
-
-    x=1
 
 if __name__ == '__main__':
     load_clean_data()
